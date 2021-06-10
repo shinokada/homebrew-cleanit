122d9562fbac819bdf1012bb65f12f1bfc4b9bd4
Shinichi Okada <okada.shin@gmail.com>
2021-06-10 17:07:36 +0900

    v0.2.1

--- cleanit.rb
+++ cleanit.rb
@@ -1,7 +1,7 @@
 class Cleanit < Formula
     desc "Cleanit moves files older than 20 days (default) from a directory (default ~/Downloads) to another directory (default ~/.Trash)."
-    url "https://github.com/shinokada/cleanit/archive/refs/tags/v0.2.0.tar.gz"
-    sha256 "8548c8c34dbd0ed5d3ba9c0cad129c797f2f227623ef9f2adaff570a236ee867"
+    url "https://github.com/shinokada/cleanit/archive/refs/tags/v0.2.1.tar.gz"
+    sha256 "2387d3a8a76333f9d08b1bc1e3289c07581cee67a8bec9db410e1555ed0663b4"
     license "MIT"
   
     depends_on "terminal-notifier"
