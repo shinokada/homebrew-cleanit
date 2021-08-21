e70bf7d7cb87af54edc915d344d04ff66418a576
Shinichi Okada <okada.shin@gmail.com>
2021-08-22 08:22:14 +0900

    version 0.4.0

--- cleanit.rb
+++ cleanit.rb
@@ -1,14 +1,15 @@
 class Cleanit < Formula
     desc "Cleanit moves files older than 20 days (default) from a directory (default ~/Downloads) to another directory (default ~/.Trash)."
-    url "https://github.com/shinokada/cleanit/archive/refs/tags/v0.3.14.tar.gz"
-    sha256 "b724993589bde76c4cfdc295fe4d550c866f9478f6ce53324c573268d4f4e0bf"
+    url "https://github.com/shinokada/cleanit/archive/refs/tags/v0.4.0.tar.gz"
+    sha256 "41b8c26f3d874e88df7189cc5f09b0af83156be5cd64ad2b281a61d2e53734f1"
     license "MIT"
   
   
     def install
       bin.install "cleanit"
-      bin.install "cleanit-launchd"
-      bin.install "local.cleanit.plist"
+      bin.install Dir["utils"]
+      prefix.install "LICENSE"
+      prefix.install "README.md"
     end
   
     test do
